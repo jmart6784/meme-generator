e2aa30cb0fa9fc4bd4a2ea6c21ddbf0eb7ee01e5
jmart6784 <jmartinez6784@yahoo.com>
2021-03-29 19:50:43 -0700

    obtained memes throught API call

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const MemeGenerator = () => {
-  return (
-    <div>
-      <h1>MEME</h1>
-    </div>
-  );
+  const [memes, setMemes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const randomNumber = (myMin, myMax) => {
+    return Math.floor(
+      Math.random() * (Math.ceil(myMax) - Math.floor(myMin) + 1) + myMin
+    );
+  };
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((response) => {
+        setMemes(response.data.memes);
+        console.log(response.data, memes);
+        setLoading(false);
+      });
+  }, []);
+
+  if (!loading) {
+    return (
+      <div>
+        <h1>MEME {memes[randomNumber(0, memes.length - 1)].name}</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>LOADING...</h1>
+      </div>
+    );
+  }
 };
 
 export default MemeGenerator;
